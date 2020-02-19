3ebf7ae086215a5f780dba66ac88c429139800c3
david-kishi <dansdk.blue@gmail.com>
2020-02-19 04:16:08 -0800

    Completed ring_mod.c

--- Homework_02/ring_mod.c
+++ Homework_02/ring_mod.c
@@ -1,9 +1,7 @@
 /******************************************************************************
 * FILE: ring_mod.c
 * DESCRIPTION:
-*   Exchange one double value between process with rank 0 and process with
-*   rank 1. Calculate the execution time using MPI_Wtime for blocking & non-
-*   blocking send/receive with single & round-trip transmissions.
+*   Modified ring example to calculate execution time.
 * AUTHOR: David Nguyen
 * CONTACT: david@knytes.com
 * LAST REVISED: 19/02/2020
@@ -13,10 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-    int rank, size;
+    double  start,  // Start Time
+            stop;   // Stop Time
+    int     rank,   // Process rank number
+            size;   // Number of processes
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     printf("I am %d of %d\n", rank, size);
 
     int token;
@@ -28,16 +30,23 @@ int main(int argc, char *argv[])
     else
     {
         // Set the token's value if you are process 0
-        token = -1;
+        token = 5;
     }
     MPI_Send(&token, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
 
     // Now process 0 can receive from the last process.
     if (rank == 0)
     {
+        start = MPI_Wtime(); // Start Timer
+
         MPI_Recv(&token, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        stop = MPI_Wtime(); // End Timer
+        
         printf("Process %d received token %d from process %d\n", rank, token, size - 1);
+
+        /* Print execution time */
+        printf("\tExecution Time: %f\n", stop - start);
     }
     MPI_Finalize();
-    return 0;
 }
