46fd852dab73c6f16870cf605b308d3a79fb2d1f
david-kishi <dansdk.blue@gmail.com>
2020-03-20 19:48:06 -0700

    Initiated homework 5

---
+++ Homework/05/simple_cuda.cu
@@ -0,0 +1,18 @@
+/******************************************************************************
+* FILE: simple_cuda.cu
+* DESCRIPTION:
+*   A simple cuda program working with arrays.
+* AUTHOR: David Nguyen
+* CONTACT: david@knytes.com
+* LAST REVISED: 20/03/2020
+******************************************************************************/
+#include <stdio.h>
+#include <cuda.h>
+
+
+int main(){
+
+    
+    cudaThreadSynchronize();
+    return 0;
+}
