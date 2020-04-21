f6a9c1477d6be100621c136fd49e119dd1a756c4
david_kishi <dansdk.blue@gmail.com>
2020-04-20 22:38:58 -0700

    Completed Homework 06 - CUDA

--- Homework/06/cuda_matrix.cu
+++ Homework/06/cuda_matrix.cu
@@ -12,79 +12,90 @@
 
 #define BLOCK_SIZE  1024
 #define N           32
-#define NUM         4
+#define NUM         10
 
 
 
-// Host function to set values of matrix to one integer
-__host__
-void numSet(int *mat){
-    for(int i = 0; i < N*N; i++){
-        mat[i] = NUM;
+// Global function for Matrix Squaring
+__global__
+void deviceMatrixSquare(unsigned *mat_d, unsigned *p_mat_d, int width){
+    unsigned p_value = 0;
+    for(int k = 0; k < width; ++k){
+        unsigned element = mat_d[threadIdx.y*width+k];
+        p_value = element * element;
+        p_mat_d[threadIdx.y*width+threadIdx.x] = p_value;
     }
 }
 
-
-// Global function for computation
-__global__
-void addI(int *d_array, int N){
-    unsigned id = blockIdx.x * blockDim.x + threadIdx.x;
-    if(id < N) {d_array[id] += id;}
-}
-
-
-int main(){
-    // Variable declaration
-    unsigned *mat, *d_mat;
+// Host function to process 
+__host__
+void matrixSquare(unsigned *mat, unsigned *p_mat){
+    unsigned size = N*N*sizeof(unsigned);
+    unsigned *mat_d, *p_mat_d;
 
     // Determine number of blocks and blocksize
     // if N <= 1024, then nBlocks = 1 & nThreads = N
-    // unsigned nBlocks = ceil(((float)N*N)/BLOCK_SIZE); // number of blocks needed
-    // unsigned nThreads = N*N <= BLOCK_SIZE ? N*N : BLOCK_SIZE; // determine threads
+    unsigned nBlocks = ceil(((float)N*N)/BLOCK_SIZE); // number of blocks needed
+    unsigned nThreads = N*N <= BLOCK_SIZE ? N*N : BLOCK_SIZE; // determine threads
+
+    // Memory Allocation
+    cudaMalloc(&mat_d, size);
+    cudaMalloc(&p_mat_d, size);
+
+    // Load mat_d to device memory
+    cudaMemcpy(mat_d, mat, size, cudaMemcpyHostToDevice);
+
+    // Device Function - Matrix Squaring
+    deviceMatrixSquare<<<nBlocks, nThreads>>>(mat_d, p_mat_d, N);
+
+    // Copy result from device memory
+    cudaMemcpy(p_mat, p_mat_d, size, cudaMemcpyDeviceToHost);
+
+    // Free memory
+    cudaFree(mat_d);
+    cudaFree(p_mat_d);
+}
+
+int main(){
+    // Variable declaration
+    unsigned *mat, *p_mat;
 
     // Memory Allocation
     mat = (unsigned*)malloc(N*N*sizeof(unsigned));
-    // cudaMalloc(&d_mat, N*N*sizeof(unsigned));
+    p_mat = (unsigned*)malloc(N*N*sizeof(unsigned));
 
-    // // Copy Contents of Host Array to Device Array
-    // cudaMemcpy(d_mat, mat, N*N*sizeof(unsigned), cudaMemcpyHostToDevice);
+    // Set all matrix values to NUM
+    for(int i = 0; i < N*N; i++){
+        mat[i] = NUM;
+    }
 
-    
-
-    // Print array before zero-setting to check values
-    printf("BEFORE NUM-SETTING\n");
+    // Print array after num-setting to check values
+    printf("Matrix\n");
     for(int i = 0; i < N*N; i++){
         printf(" %d", mat[i]);
+        // Newline if end of row
+        if((i+1)%N==0){
+            printf("\n");
+        }
     }
     printf("\n");
 
-    numSet(mat);
+    // Call matrix squaring function
+    matrixSquare(mat, p_mat);
 
-    // // Call device function to set zeros then copy back to host memory
-    // zeroSet<<<nBlocks, nThreads>>>(d_array, N);
-    // cudaMemcpy(array, d_array, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-    // Print array after zero-setting to check values
-    printf("AFTER NUM_SETTING\n");
+    // Print array after num-setting to check values
+    printf("AFTER MATRIX SQUARING\n");
     for(int i = 0; i < N*N; i++){
-        printf(" %d", mat[i]);
+        printf(" %d", p_mat[i]);
+        // Newline if end of row
+        if((i+1)%N==0){
+            printf("\n");
+        }
     }
     printf("\n");
 
-    // // Call device function to compute then copy back to host memory
-    // addI<<<nBlocks, nThreads>>>(d_array, N);
-    // cudaMemcpy(array, d_array, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-    // // Print array after computation to check values
-    // printf("AFTER ADDING I\n");
-    // for(int i = 0; i < N; i++){
-    //     printf(" %d", array[i]);
-    // }
-    // printf("\n");
-
-    // Free the memory
-    cudaFree(d_array);
-    free(array);
+    // Free memory
+    free(mat);
 
     return 0;
 }
